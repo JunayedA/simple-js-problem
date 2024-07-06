9402f2d433f966930fbe505fc948fab1c6024d3d
JunayedA <junayedahmed11099@gmail.com>
2024-07-06 20:54:33 +0600

    array slice and splice

---
+++ explore-more-js-problem/slice.js
@@ -0,0 +1,4 @@
+const friends = [34,45,432,56,23,56,23,56,12,56]
+
+const slice = friends.slice(3,5)
+console.log(slice);
---
+++ explore-more-js-problem/splice.js
@@ -0,0 +1,4 @@
+const friends = [34,45,432,56,23,56,23,56,12,56];
+const partial = friends.splice(1,3,222,456,777);
+console.log(partial);
+console.log(friends);
