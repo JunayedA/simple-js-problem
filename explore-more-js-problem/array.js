a1569f8489f3b8cc4df0c9b81fcf73c7a400b4ea
JunayedA <junayedahmed11099@gmail.com>
2024-07-06 20:24:00 +0600

    next day more js simple problem

---
+++ explore-more-js-problem/array.js
@@ -0,0 +1,9 @@
+const myFriends =[23,23,5,54324,3423,4,34,235,232,34,23];
+const name = 'junayed Ahmed'
+console.log(typeof name)
+console.log(Array.isArray(myFriends));
+console.log(myFriends.includes(23));
+const newFriedns=[343,23432,43,23,32,34,34]
+
+const allFriends = newFriedns.concat(myFriends);
+console.log(allFriends)
